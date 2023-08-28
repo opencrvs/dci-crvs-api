c576a25be19a168d52a77d680c7334ece8575f4a
naftis <pyry.rouvila@gmail.com>
2023-08-28 15:05:27 +0300

    feat: allow disabling response mocking

--- packages/dci-api/src/constants.ts
+++ packages/dci-api/src/constants.ts
@@ -1,3 +1,6 @@
 export const HOST = process.env.HOST ?? "0.0.0.0";
 export const PORT = process.env.PORT ?? 1660;
 export const DEFAULT_TIMEOUT_MS = 600000;
+
+/** Tests */
+export const NO_RESPONSE_MOCK = Boolean(process.env.NO_RESPONSE_MOCK);
--- packages/dci-api/src/test-utilities.ts
+++ packages/dci-api/src/test-utilities.ts
@@ -1,10 +1,15 @@
 import { setupServer } from "msw/node";
 import type { SetupServer } from "msw/node";
+import { NO_RESPONSE_MOCK } from "./constants";
 
 type ServerOptions = Parameters<typeof setupServer>;
 
 export const withRequestInterception =
   (handlers: ServerOptions, test: (server: SetupServer) => any) => async () => {
+    if (NO_RESPONSE_MOCK) {
+      return await Promise.resolve(test(setupServer()));
+    }
+
     const server = setupServer(...handlers);
     server.listen();
 
