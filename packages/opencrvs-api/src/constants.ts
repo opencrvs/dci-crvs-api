9ecfa250b9f4b219eabcde169b0c979b6ddc904a
Pyry <pyry.rouvila@gmail.com>
2023-09-21 14:49:49 +0300

    feat: redirect OAuth requests to OpenCRVS auth

--- packages/opencrvs-api/src/constants.ts
+++ packages/opencrvs-api/src/constants.ts
@@ -1,4 +1,8 @@
+/** Used to get JWT information such as .well-known to ensure JWT's are valid */
 export const OPENCRVS_AUTH_URL =
   process.env.OPENCRVS_AUTH_URL ?? 'http://localhost:4040/'
+/** Used for record search */
 export const OPENCRVS_GATEWAY_URL =
   process.env.OPENCRVS_GATEWAY_URL ?? 'http://localhost:7070/graphql'
+/** Used to redirect API request directly to core */
+export const OPENCRVS_TOKEN_URL = new URL('token', OPENCRVS_AUTH_URL)
--- packages/opencrvs-api/src/index.ts
+++ packages/opencrvs-api/src/index.ts
@@ -1,2 +1,8 @@
 export * from './types'
-export { OPENCRVS_AUTH_URL, OPENCRVS_GATEWAY_URL } from './constants'
+export {
+  OPENCRVS_AUTH_URL,
+  OPENCRVS_GATEWAY_URL,
+  OPENCRVS_TOKEN_URL
+} from './constants'
+export * from './auth'
+export * from './search'
