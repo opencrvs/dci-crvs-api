ce54af340d86607ed451b264bbc606276a5a2e0d
Tameem Bin Haider <haidertameem@gmail.com>
2023-10-03 18:15:25 +0600

    Include "use" parameter in JWK

--- packages/dci-api/src/jwks/handler.ts
+++ packages/dci-api/src/jwks/handler.ts
@@ -1,16 +1,20 @@
 import { type JWK, type KeyLike, calculateJwkThumbprint, exportJWK } from 'jose'
 import { getEncryptionKeys, getSigningKeys } from '../crypto/keys'
 
-async function getJwk(publicKey: KeyLike): Promise<JWK> {
+async function getJwk(publicKey: KeyLike, use: 'sig' | 'enc'): Promise<JWK> {
   const jwk = await exportJWK(publicKey)
   jwk.kid = await calculateJwkThumbprint(jwk)
+  jwk.use = use
   return jwk
 }
 
 export async function getJwksHandler() {
   const [{ publicKey: signingKey }, { publicKey: encryptionKey }] =
     await Promise.all([getSigningKeys(), getEncryptionKeys()])
-  const keys = await Promise.all([getJwk(signingKey), getJwk(encryptionKey)])
+  const keys = await Promise.all([
+    getJwk(signingKey, 'sig'),
+    getJwk(encryptionKey, 'enc')
+  ])
   return {
     keys
   }
