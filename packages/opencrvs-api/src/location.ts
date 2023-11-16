cd9117e38cc54e13d84d62f4a8abf6ef20f2b4a8
naftis <pyry.rouvila@gmail.com>
2023-11-16 14:14:15 +0200

    fix: location api error handling

--- packages/opencrvs-api/src/location.ts
+++ packages/opencrvs-api/src/location.ts
@@ -1,8 +1,8 @@
 import { OPENCRVS_FHIR_LOCATION_API } from './constants'
 import type { SavedLocation, Bundle } from '@opencrvs/commons/build/dist/fhir'
 
-const ADMIN_STRUCTURE = new URL(OPENCRVS_FHIR_LOCATION_API)
-ADMIN_STRUCTURE.searchParams.append('type', 'ADMIN_STRUCTURE')
+const ADMIN_STRUCTURE_API = new URL(OPENCRVS_FHIR_LOCATION_API)
+ADMIN_STRUCTURE_API.searchParams.append('type', 'ADMIN_STRUCTURE')
 
 const HEALTH_FACILITIES_API = new URL(OPENCRVS_FHIR_LOCATION_API)
 HEALTH_FACILITIES_API.searchParams.append('type', 'HEALTH_FACILITY')
@@ -11,7 +11,7 @@ const CRVS_OFFICE_API = new URL(OPENCRVS_FHIR_LOCATION_API)
 CRVS_OFFICE_API.searchParams.append('type', 'CRVS_OFFICE')
 
 export async function fetchFHIRLocations() {
-  const adminStructureRequest = fetch(OPENCRVS_FHIR_LOCATION_API)
+  const adminStructureRequest = fetch(ADMIN_STRUCTURE_API)
   const healthFacilitiesRequest = fetch(HEALTH_FACILITIES_API)
   const crvsOfficeRequest = fetch(CRVS_OFFICE_API)
 
@@ -23,18 +23,32 @@ export async function fetchFHIRLocations() {
 
   if (!adminStructure.ok) {
     throw new Error(
-      'Something went wrong in FHIR Location: Admin Structure API'
+      `Something went wrong in FHIR Location: Admin Structure API: ${JSON.stringify(
+        await adminStructure.json(),
+        null,
+        4
+      )}`
     )
   }
 
   if (!healthFacilities.ok) {
     throw new Error(
-      'Something went wrong in FHIR Location: Health Facilities API'
+      `Something went wrong in FHIR Location: Health Facilities API: ${JSON.stringify(
+        await healthFacilities.json(),
+        null,
+        4
+      )}`
     )
   }
 
   if (!crvsOffice.ok) {
-    throw new Error('Something went wrong in FHIR Location: CRVS Office API')
+    throw new Error(
+      `Something went wrong in FHIR Location: CRVS Office API: ${JSON.stringify(
+        await crvsOffice.json(),
+        null,
+        4
+      )}`
+    )
   }
 
   const adminStructureJson =
